8e7fe7c7023603967c3eebc698b4271dc1494b8c
Robert WDF <robert.wdf@protonmail.com>
2026-04-29 14:53:50 +0200

    kubebuilder
    lots of boilerplate and crap in here

--- api/v1alpha1/testrun_types.go
+++ api/v1alpha1/testrun_types.go
@@ -1,23 +1,92 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TestRunSpec defines the desired state of TestRun
 type TestRunSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// The following markers will use OpenAPI v3 schema to validate the value
+	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+
 	Service     string `json:"service"`
 	Version     string `json:"version"`
 	CallbackURL string `json:"callbackURL,omitempty"` // omitempty? if not tied to a pipe but only running on cron
 }
 
+// TestRunStatus defines the observed state of TestRun.
 type TestRunStatus struct {
-	Phase   string `json:"phase"`
-	Promote bool   `json:"promote"`
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the TestRun resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+
+// TestRun is the Schema for the testruns API
 type TestRun struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              TestRunSpec   `json:"spec,omitempty"`
-	Status            TestRunStatus `json:"status,omitempty"`
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of TestRun
+	// +required
+	Spec TestRunSpec `json:"spec"`
+
+	// status defines the observed state of TestRun
+	// +optional
+	Status TestRunStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// TestRunList contains a list of TestRun
+type TestRunList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []TestRun `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TestRun{}, &TestRunList{})
 }
